60ca73e6a60ef6683d1acb10455d6eff9ee1de5f
Martin Borzan <martin.borzan@yahoo.com>
2020-09-07 00:08:40 +0200

    Added 2 conditional scope functions

---
+++ src/main/kotlin/ScopeFunctions.kt
@@ -0,0 +1,28 @@
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
+
+/**
+ * Calls the specified function [block] with `this` value as its argument if the [condition] is met,
+ * and returns its result. If the condition is not met, simply returns `this` value
+ */
+@OptIn(ExperimentalContracts::class)
+inline fun <R> R.modifyIf(condition: Boolean, block: (R) -> R): R {
+  contract {
+    callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+  }
+  return if(condition) block(this) else this
+}
+
+/**
+ * Calls the specified function [block] with `this` value as its receiver if the [condition] is met,
+ * and returns its result. If the condition is not met, simply returns `this` value
+ */
+@OptIn(ExperimentalContracts::class)
+inline fun <R> R.transformIf(condition: Boolean, block: () -> R): R {
+  contract {
+    callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+  }
+  return if(condition) block() else this
+}
